c2f2a5d8da17473da5d986385737fe8e1980d046
jha0517 <jha0517@hotmail.com>
2019-05-01 22:50:18 +0200

    initial

---
+++ ProjetVR/Assets/Hyunah/Script/controllerScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class controllerScript : MonoBehaviour
+{
+    public GameObject go;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {   RaycastHit hit;
+        transform.rotation = OVRInput.GetLocalControllerRotation(OVRInput.Controller.RTrackedRemote);
+
+        if (Physics.Raycast(transform.position, transform.forward, out hit))
+        { 
+            if (hit.collider != null)
+            {
+                if(go != hit.transform.gameObject)
+                {
+                    go = hit.transform.gameObject;
+                    Debug.Log("Raycast Enter");
+                }
+            } 
+        }
+    }
+}
---
+++ ProjetVR/Assets/Hyunah/Script/tutorialGaze.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tutorialGaze : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void GazeEnter()
+    {
+        Debug.Log("Enter");
+    }
+    public void GazeExit()
+    {
+        Debug.Log("Exit");
+
+    }
+}
